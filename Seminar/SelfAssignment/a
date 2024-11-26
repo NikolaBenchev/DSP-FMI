7adbaca565eb25dd1c4c3022f458b84a7bc64d4a
koliobenchev2@gmail.com <koliobenchev2@gmail.com>
2024-11-26 18:52:19 +0200

    self assignment vector and LL problem started

---
+++ Seminar/SelfAssignment/a
@@ -0,0 +1,9 @@
+typename <class T>
+class Vector {
+	public:
+		Vector() {}
+		~Vector
+	
+	private:
+		arr[];
+};
---
+++ Seminar/Week 7/main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+struct Node {
+	int value;
+	Node* right;
+	Node* bottom;
+};
+
+int main() {
+	int matrix[4][4] = {
+		{11, 12, 13, 14},
+		{21, 22, 23, 24},
+		{31, 32, 33, 34},
+		{41, 42, 43, 44}
+	};
+	//matrix -> linkedList;
+	return 0;
+}
